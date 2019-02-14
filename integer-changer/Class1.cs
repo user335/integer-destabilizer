69b86847184c70447803c0adb17a271000429542
Andy Weekes <userthreethreefive@gmail.com>
2019-02-13 18:43:09 -0700

    initial commit

---
+++ integer-changer/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integer_changer
+{
+    class Class1
+    {
+        public static int ChangeMyIntA(int input)
+        {
+            return input + 3;
+        }
+        public static int ChangeMyIntB(int input)
+        {
+            return Convert.ToInt32(Math.Round(Math.Log10(Math.Pow(input + 3, 2))));
+        }
+        static int usageCounter = 1;
+        public static int ChangeMyIntC(int input)
+        {
+            usageCounter--;
+            if (usageCounter <= 0)
+                usageCounter = 4;
+            return input * (4 - usageCounter);
+        }
+    }
+}
---
+++ integer-changer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using integer_changer;
+
+namespace integer_changer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Having fun with math");
+            int answer = PromptForInteger();
+            Console.WriteLine("Oh, you like " + answer + ", do you? Just wait til you see what I'm going to do with " + answer);
+            Console.WriteLine("Function1(" + answer + ") = " + Class1.ChangeMyIntA(answer).ToString());
+            Console.WriteLine("Function2(" + answer + ") = " + Class1.ChangeMyIntB(answer).ToString());
+            Console.WriteLine("Function3(" + answer + ") = " + Class1.ChangeMyIntC(answer).ToString());
+            Console.WriteLine("How do you like that? Ha. Your precious " + answer + " ain't so tough.");
+            Console.Read();
+        }
+        static int PromptForInteger()
+        {
+            Console.Write("What's your favorite integer?");
+            int usersFavoriteInteger = 0;
+            bool goodAnswerReceived = false;
+            while (goodAnswerReceived == false)
+                try
+                {
+                    usersFavoriteInteger = Convert.ToInt32(Console.ReadLine());
+                    goodAnswerReceived = true;
+                    return usersFavoriteInteger;
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("That was not an integer, please give me your favorite integer. I need it to have fun");
+                }
+            return 0;
+        }
+    }
+}
